c230d5a34dc7938f9cd6a2bac52f84c440edcf36
Sergey Ponomarev <stokito@gmail.com>
2013-02-23 16:54:41 +0200

    Move method amontFor

--- src/main/java/org/kranonit/rentalStore/Customer.java
+++ src/main/java/org/kranonit/rentalStore/Customer.java
@@ -25,7 +25,7 @@ class Customer {
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-			double thisAmount = amountFor(each);
+			double thisAmount = each.amountFor();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -44,24 +44,4 @@ class Customer {
         return result;
     }
 
-	public double amountFor(Rental aRental) {
-		double thisAmount = 0;
-		// determine amounts for each line
-		switch (aRental.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (aRental.getDaysRented() > 2)
-					thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += aRental.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (aRental.getDaysRented() > 3)
-					thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-				break;
-		}
-		return thisAmount;
-	}
 }
--- src/main/java/org/kranonit/rentalStore/Rental.java
+++ src/main/java/org/kranonit/rentalStore/Rental.java
@@ -11,7 +11,28 @@ class Rental {
         _daysRented = daysRented;
     }
 
-    public int getDaysRented() {
+	public double amountFor() {
+		double thisAmount = 0;
+		// determine amounts for each line
+		switch (getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (getDaysRented() > 2)
+					thisAmount += (getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (getDaysRented() > 3)
+					thisAmount += (getDaysRented() - 3) * 1.5;
+				break;
+		}
+		return thisAmount;
+	}
+
+	public int getDaysRented() {
         return _daysRented;
     }
 
