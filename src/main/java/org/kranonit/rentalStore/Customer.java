197edd321e781538813c64b66a15a6fbb75868d7
grandis <grandis.kepr@gmail.com>
2013-02-24 13:28:24 +0200

    Выделение начисления бонусов в метод.

--- src/main/java/org/kranonit/rentalStore/Customer.java
+++ src/main/java/org/kranonit/rentalStore/Customer.java
@@ -25,12 +25,8 @@ class Customer {
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-			// add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
+
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t"
                     + String.valueOf(each.getCharge()) + "\n";
@@ -42,5 +38,6 @@ class Customer {
                 + " frequent renter points";
         return result;
     }
+
 
 }
