54b400b1016e245b859efb4e40573aec9822538c
Clicker <pavel.shchelkun@gmail.com>
2013-02-24 19:56:46 +0200

    Extract Method.

--- src/main/java/org/kranonit/rentalStore/Customer.java
+++ src/main/java/org/kranonit/rentalStore/Customer.java
@@ -26,11 +26,7 @@ class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += each.getFrequentedRenterPoints();
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t"
                     + String.valueOf(each.getCharge()) + "\n";
--- src/main/java/org/kranonit/rentalStore/Rental.java
+++ src/main/java/org/kranonit/rentalStore/Rental.java
@@ -38,4 +38,11 @@ class Rental {
         }
         return result;
     }
+
+    public int getFrequentedRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
