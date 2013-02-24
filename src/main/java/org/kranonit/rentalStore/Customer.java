9bd656fca875b5dbb360432b00ba37b04d8ea823
grandis <grandis.kepr@gmail.com>
2013-02-24 13:10:24 +0200

    Удаление временной переменной thisAmount.

--- src/main/java/org/kranonit/rentalStore/Customer.java
+++ src/main/java/org/kranonit/rentalStore/Customer.java
@@ -25,8 +25,7 @@ class Customer {
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-			double thisAmount = each.getCharge();
-            // add frequent renter points
+			// add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
@@ -34,8 +33,8 @@ class Customer {
                 frequentRenterPoints++;
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t"
-                    + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
         // add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
