3bb0536c9c351cc5366efb7cef8e0c3d7f050575
grandis <grandis.kepr@gmail.com>
2013-02-25 12:27:20 +0200

    Вычисление getFrequentRenterPoints и getCharge в классе Movie.

--- src/main/java/org/kranonit/rentalStore/Movie.java
+++ src/main/java/org/kranonit/rentalStore/Movie.java
@@ -23,4 +23,34 @@ public class Movie {
     public String getTitle() {
         return _title;
     }
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+            }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
--- src/main/java/org/kranonit/rentalStore/Rental.java
+++ src/main/java/org/kranonit/rentalStore/Rental.java
@@ -12,33 +12,12 @@ class Rental {
     }
 
 	public double getCharge() {
-		double thisAmount = 0;
-		// determine amounts for each line
-		switch (getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (getDaysRented() > 2)
-					thisAmount += (getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (getDaysRented() > 3)
-					thisAmount += (getDaysRented() - 3) * 1.5;
-				break;
-		}
-		return thisAmount;
+        return _movie.getCharge(_daysRented);
 	}
 
     public int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                && getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+
+        return  _movie.getFrequentRenterPoints(_daysRented);
     }
 
     public int getDaysRented() {
