c301114958ba9497e8461b85f91d7ed40f3d6d7e
grandis <grandis.kepr@gmail.com>
2013-02-25 13:28:05 +0200

    Добавление класса Price.

---
+++ src/main/java/org/kranonit/rentalStore/Price.java
@@ -0,0 +1,23 @@
+package org.kranonit.rentalStore;
+
+abstract class Price {
+    abstract int getPriceCode();
+}
+
+class ChildrensPrice extends Price {
+    int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+}
+
+class NewReleasePrice extends Price {
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+}
+
+class RegularPrice extends Price {
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+}
