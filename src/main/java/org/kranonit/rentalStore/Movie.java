0652b2769c050a67b5c470071e228fd3ca71e643
Anna Lutsko <luspena@gmail.com>
2013-02-26 20:03:40 +0200

    Move method ( getCharge and getFrequentRenterPoints move to Price )

--- src/main/java/org/kranonit/rentalStore/Movie.java
+++ src/main/java/org/kranonit/rentalStore/Movie.java
@@ -37,29 +37,10 @@ public class Movie {
     }
 
     public double getCharge( int daysRented ) {
-        double thisAmount = 0;
-        // determine amounts for each line
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints( int daysRented ){
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
