76cfc0ebaba12be7c08980a00f8b45ecbf5eaae2
Anna Lutsko <luspena@gmail.com>
2013-02-26 19:43:54 +0200

    Move method ( getFrequentRenterPoints() to Movie )

--- src/main/java/org/kranonit/rentalStore/Movie.java
+++ src/main/java/org/kranonit/rentalStore/Movie.java
@@ -44,4 +44,10 @@ public class Movie {
         }
         return thisAmount;
     }
+
+    public int getFrequentRenterPoints( int daysRented ){
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            return 2;
+        return 1;
+    }
 }
--- src/main/java/org/kranonit/rentalStore/Rental.java
+++ src/main/java/org/kranonit/rentalStore/Rental.java
@@ -16,9 +16,7 @@ class Rental {
 	}
 
     public int getFrequentRenterPoints(){
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            return 2;
-        return 1;
+       return _movie.getFrequentRenterPoints( getDaysRented() );
     }
 
 	public int getDaysRented() {
