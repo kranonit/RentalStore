be070fd265158d26dc2ffdb0c5c8237e734b3c4c
Anna Lutsko <luspena@gmail.com>
2013-02-26 19:40:54 +0200

    Move method ( getCharge() to Movie )

--- src/main/java/org/kranonit/rentalStore/Movie.java
+++ src/main/java/org/kranonit/rentalStore/Movie.java
@@ -23,4 +23,25 @@ public class Movie {
     public String getTitle() {
         return _title;
     }
+
+    public double getCharge( int daysRented ) {
+        double thisAmount = 0;
+        // determine amounts for each line
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
 }
--- src/main/java/org/kranonit/rentalStore/Rental.java
+++ src/main/java/org/kranonit/rentalStore/Rental.java
@@ -12,24 +12,7 @@ class Rental {
     }
 
 	public double getCharge() {
-		double thisAmount = 0;
-		// determine amounts for each line
-		switch (getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (getDaysRented() > 2)
-					thisAmount += (getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (getDaysRented() > 3)
-					thisAmount += (getDaysRented() - 3) * 1.5;
-				break;
-		}
-		return thisAmount;
+        return _movie.getCharge( getDaysRented() );
 	}
 
     public int getFrequentRenterPoints(){
