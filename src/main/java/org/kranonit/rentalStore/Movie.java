b8cc7300c02e963cb2085a1b14b1e6353172961d
Clicker <pavel.shchelkun@gmail.com>
2013-02-28 00:22:18 +0200

    Move Method.

--- src/main/java/org/kranonit/rentalStore/Movie.java
+++ src/main/java/org/kranonit/rentalStore/Movie.java
@@ -39,24 +39,7 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double result = 0;
-        // determine amounts for each line
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
