9481549b4c1fdac061a27522802c597eec46600f
Clicker <pavel.shchelkun@gmail.com>
2013-02-28 00:05:35 +0200

    Replace Type Code with State/Strategy

--- src/main/java/org/kranonit/rentalStore/Movie.java
+++ src/main/java/org/kranonit/rentalStore/Movie.java
@@ -7,7 +7,7 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
@@ -15,11 +15,23 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
